4cd90b8b39f140ddd33d0242afeb5da6168a510f
Nickolas Burr <nickolasburr@gmail.com>
2020-02-16 16:47:31 -0500

    Add ModuleNotEnabledException throwable class

---
+++ Exception/ModuleNotEnabledException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * ModuleNotEnabledException.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/notificationservice/LICENSE.txt
+ *
+ * @package       AuroraExtensions_NotificationService
+ * @copyright     Copyright (C) 2020 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\NotificationService\Exception;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class ModuleNotEnabledException extends LocalizedException
+{
+}
